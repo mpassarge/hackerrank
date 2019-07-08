08c15b9f25941c6f5c2f4e1394ce00f503f03d46
Michael Passarge <mpassarge@comcast.net>
2019-07-08 14:10:54 -0400

    Added initial example tests

--- repeated-string/src/test/java/com/passargecorp/repeat/TestRepeatString.java
+++ repeated-string/src/test/java/com/passargecorp/repeat/TestRepeatString.java
@@ -1,11 +1,25 @@
 package com.passargecorp.repeat;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestRepeatString {
 
     @Test
-    public void testRepeatString_() {
+    public void testRepeatString_example1() {
 
+        String s = "aba";
+        long n = 10L;
+
+        Assert.assertEquals(7, RepeatString.repeatedString(s, n));
+    }
+
+    @Test
+    public void testRepeatString_example2() {
+
+        String s = "a";
+        long n = 1000000000000L;
+
+        Assert.assertEquals(1000000000000L, RepeatString.repeatedString(s, n));
     }
 }
