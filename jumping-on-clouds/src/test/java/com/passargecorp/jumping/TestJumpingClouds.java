1cdd30d82044a971732ab2683ad15ce4900f9186
Michael Passarge <mpassarge@comcast.net>
2019-07-08 10:58:29 -0400

    Added test cases for jumping clouds

--- jumping-on-clouds/src/test/java/com/passargecorp/jumping/TestJumpingClouds.java
+++ jumping-on-clouds/src/test/java/com/passargecorp/jumping/TestJumpingClouds.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class TestJumpingClouds {
 
     @Test
-    public void testJumpingOnClouds_1() {
+    public void testJumpingOnClouds_example1() {
 
         int[] c = {0, 0, 1, 0, 0, 1, 0};
 
@@ -14,10 +14,34 @@ public class TestJumpingClouds {
     }
 
     @Test
-    public void testJumpingOnClouds_2() {
+    public void testJumpingOnClouds_example2() {
 
         int[] c = {0, 0, 0, 0, 1, 0};
 
         Assert.assertEquals(3, JumpingClouds.jumpingOnClouds(c));
+    }
+
+    @Test
+    public void testJumpingOnClouds_noClouds() {
+
+        int[] c = {};
+
+        Assert.assertEquals(0, JumpingClouds.jumpingOnClouds(c));
+    }
+
+    @Test
+    public void testJumpingOnClouds_allNoGoClouds() {
+
+        int[] c = {1, 1, 1, 1, 1};
+
+        Assert.assertEquals(0, JumpingClouds.jumpingOnClouds(c));
+    }
+
+    @Test
+    public void testJumpingOnClouds_allJumpingClouds() {
+
+        int[] c = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        Assert.assertEquals(5, JumpingClouds.jumpingOnClouds(c));
     }
 }
