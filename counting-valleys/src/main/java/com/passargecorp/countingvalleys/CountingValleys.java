1aceaec27ee5654c70a3a5bb8675a464023208b0
Michael Passarge <mpassarge@comcast.net>
2019-07-08 08:45:36 -0400

    Finished solution to counting valleys

--- counting-valleys/src/main/java/com/passargecorp/countingvalleys/CountingValleys.java
+++ counting-valleys/src/main/java/com/passargecorp/countingvalleys/CountingValleys.java
@@ -1,7 +1,40 @@
 package com.passargecorp.countingvalleys;
 
-public class CountingValleys{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+public class CountingValleys {
+
+    static int countingValleys(int n, String s) {
+
+        boolean belowSeaLevel = false;
+        int height = 0;
+        int valleyCount = 0;
+
+        for(char c : s.toCharArray()) {
+
+            height += upOrDown(c);
+
+            // Coming back to sea level from below sea level
+            // Means coming out of Valley
+            if(belowSeaLevel && height >= 0) {
+                valleyCount++;
+                belowSeaLevel = false;
+            }
+
+            if(height < 0) {
+                belowSeaLevel = true;
+            }
+        }
+        return valleyCount;
+    }
+
+    // Returns +/- whether character is U(Up/+1) or D(Down/-1)
+    // Used to find the level in the hike
+    static int upOrDown(char c) {
+
+        if(c == 'U') {
+            return 1;
+        } else if(c == 'D') {
+            return -1;
+        }
+        return 0;
     }
 }
--- counting-valleys/src/test/java/com/passargecorp/countingvalleys/TestCountingValleys.java
+++ counting-valleys/src/test/java/com/passargecorp/countingvalleys/TestCountingValleys.java
@@ -1,10 +1,51 @@
 package com.passargecorp.countingvalleys;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class TestCountingValleys{
+public class TestCountingValleys {
 
+    /*
+        _/\      _
+           \    /
+            \/\/
+
+    */
     @Test
-    public void test(){
+    public void testCountingValleys() {
+
+        String s = "UDDDUDUU";
+        int n = s.length();
+
+        Assert.assertEquals(1, CountingValleys.countingValleys(n, s));
+    }
+
+    /*
+            /\
+        _/\/  \    /\    _
+               \  /  \  /
+                \/    \/
+    */
+    @Test
+    public void testCountingValleys_multipleValleys() {
+
+        String s = "UDUUDDDDUUUDDDUU";
+        int n = s.length();
+
+        Assert.assertEquals(2, CountingValleys.countingValleys(n, s));
+    }
+
+    /*
+      _
+       \  _
+        \/
+    */
+    @Test
+    public void testCountingValleys_noValleys() {
+
+        String s = "DDU";
+        int n = s.length();
+
+        Assert.assertEquals(0, CountingValleys.countingValleys(n, s));
     }
 }
